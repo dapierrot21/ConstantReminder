bda739f0e3676248f5739faf35dc1abef036ce58
Dean Pierrot <dapierrot21@gmail.com>
2022-06-02 17:58:43 -0400

    Implemented register user functionalility

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,12 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 // @desc:  Register a new user
 // @route: /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email } = req.body;
+  const { name, email, password } = req.body;
 
   //Validation
   if (!name || !email || !password) {
@@ -12,9 +14,35 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please enter all fields.");
   }
 
-  res
-    .status(200)
-    .json({ message: "Local register route is working from user controller." });
+  // Does user exist.
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hasdedPassword = await bcrypt.hash(password, salt);
+
+  // New User
+  const user = await User.create({
+    name,
+    email,
+    password: hasdedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new error("User data incorrect.");
+  }
 });
 
 // @desc:  Login a user
