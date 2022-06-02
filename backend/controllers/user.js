3089763b2ff49e3cb83f13dc3c4a95e37a6ab911
Dean Pierrot <dapierrot21@gmail.com>
2022-06-02 17:16:53 -0400

    user controller to register and login.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,14 +1,30 @@
-const registerUser = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+// @desc:  Register a new user
+// @route: /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email } = req.body;
+
+  //Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all fields.");
+  }
+
   res
     .status(200)
     .json({ message: "Local register route is working from user controller." });
-};
+});
 
-const loginUser = (req, res) => {
+// @desc:  Login a user
+// @route: /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: "Local login route is working from user controller." });
-};
+});
 
 module.exports = {
   registerUser,
