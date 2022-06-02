1a363c64fdebaee97d43e01ea0a28489282b9630
Dean Pierrot <dapierrot21@gmail.com>
2022-06-02 10:49:27 -0400

    set up a basic express.js server.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("entry way to our backend.")
+const express = require("express")
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Local connection is working.'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
