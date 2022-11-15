f9a41e4a846f0e0e05ab18d00ddba01d2a5f8f6d
Dean Pierrot <dapierrot21@gmail.com>
2022-11-15 00:43:38 -0500

    fix typo

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode);
   res.json({
     message: err.message,
-    // If this is env value is production make stack value null else show stack.
+    // If this env value is production make stack value null else show stack.
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
