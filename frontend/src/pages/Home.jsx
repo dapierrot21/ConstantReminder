0fca6af552a6bd074a2b2d7ec165b80e84f12e9b
Dean Pierrot <dapierrot21@gmail.com>
2022-11-15 23:24:52 -0500

    Create Home page UI.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="heading">
+        <h1>Why are you here?</h1>
+        <p>Please enter your reason below</p>
+      </section>
+
+      <Link to="/new-reminder" className=" btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create New Reminder
+      </Link>
+      <Link to="/reminder" className=" btn btn-block">
+        <FaTicketAlt /> Reminders
+      </Link>
+    </>
+  );
 }
 
 export default Home;
