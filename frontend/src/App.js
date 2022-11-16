26fcd9c849417afe82c5fe057e2924be7fbcb007
Dean Pierrot <dapierrot21@gmail.com>
2022-11-15 23:23:25 -0500

    Swicth route login and added ToastContainer.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -12,11 +14,12 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
